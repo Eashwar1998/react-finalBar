2b8a6427d3de4ec9a091ba2f9cd66cba5d1a3052
Eashwar1998 <30700208+Eashwar1998@users.noreply.github.com>
2023-04-21 10:59:50 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -17,37 +17,94 @@ export default function App() {
   ];
 
   let maxValue = 0;
+  let yesCount = 0;
+  let noCount = 0;
 
   Mydata.forEach((item) => {
     if (item.value > maxValue) {
       maxValue = item.value;
     }
+    if (item.ptamba === 'Yes') {
+      yesCount = yesCount + 1;
+    }
+    if (item.ptamba === 'No') {
+      noCount = noCount + 1;
+    }
   });
   console.log(maxValue);
+  console.log(yesCount);
+  console.log(noCount);
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-2 border p-1  text-center">P Tamba</div>
-        <div className="col-3 border p-1   text-center">Tamba Status</div>
-        <div className="col-6 border p-1   text-center">Demo Count</div>
-      </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-2 border p-1  text-center">P Tamba</div>
+          <div className="col-3 border p-1   text-center">Tamba Status</div>
+          <div className="col-6 border p-1   text-center">Demo Count</div>
+        </div>
 
-      <div className="row">
-        <div className="col-2 border p-1  text-center  "> No</div>
+        <div className="row">
+          <div className="col-2 border p-1  text-center  "> No</div>
 
-        <div className="col-9 border p-1   text-center">
-          {Mydata.map((item) => {
-            return (
-              <div className="row ">
-                <div className="col-4  p-1   text-center ">{item.name}</div>
-                <div className="col-7  p-1  text-center ">
-                  <VerticalBar total={maxValue} count={item.value} />
+          <div className="col-9 border p-1   text-center">
+            {Mydata.map((item) => {
+              return (
+                <div className="row ">
+                  <div className="col-4  p-1   text-center ">{item.name}</div>
+                  <div className="col-7  p-1  text-center ">
+                    <VerticalBar total={maxValue} count={item.value} />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <table className="table table-bordered">
+          <thead>
+            <tr className="table-active">
+              <th className="col-3">P tamba</th>
+              <th className="col-3">Tamba Status</th>
+              <th className="col-6">Demo count</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td rowSpan={`${yesCount + 1}`}>Yes</td>
+            </tr>
+            {Mydata.map((item) => {
+              if (item.ptamba === 'Yes') {
+                return (
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>
+                      {' '}
+                      <VerticalBar total={maxValue} count={item.value} />
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+            <tr>
+              <td rowSpan={`${noCount + 1}`}>No</td>
+            </tr>
+            {Mydata.map((item) => {
+              if (item.ptamba === 'No') {
+                return (
+                  <tr>
+                    <td>{item.name}</td>
+                    <td>
+                      {' '}
+                      <VerticalBar total={maxValue} count={item.value} />
+                    </td>
+                  </tr>
+                );
+              }
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
